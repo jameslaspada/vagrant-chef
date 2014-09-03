289ec97ff056f2e487789339cc5afde5c05f0f76
James La Spada <james.laspada@gmail.com>
2014-09-03 17:18:39 -0400

    Fixed chef-client and created the hello-world cookbook.

--- chef/cookbooks/hello-world/recipes/default.rb
+++ chef/cookbooks/hello-world/recipes/default.rb
@@ -6,3 +6,31 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+webapp = "hello-world"
+base_webapp_dir = node['tomcat']['webapp_dir']
+web_service = "#{node['web']['service']['name']}#{node['web']['service']['version']}"
+
+directory "#{base_webapp_dir}/#{webapp}" do
+  owner "root"
+  group "tomcat"
+  mode "0755"
+  action :create
+end
+
+template "#{base_webapp_dir}/#{webapp}/index.html" do
+  source "index.html.erb"
+  owner "root"
+  group "tomcat"
+  mode "0644"
+  variables ({
+    :body_text => "Hello World!"
+  }
+    )
+  notifies :restart, "service[tomcat6]"
+end
+
+service "#{web_service}" do
+  supports :status => true, :restart => true, :reload => true
+  action :nothing
+end
+
